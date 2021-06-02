55780178c7d4e3b7ab886fd53737c37363092dda
Vignesh R <vignesh.srgm@gmail.com>
2021-06-02 16:29:23 +0530

    initial commit

---
+++ src/main/java/com/cs/csservermanager/CsservermanagerApplication.java
@@ -0,0 +1,20 @@
+package com.cs.csservermanager;
+
+import com.cs.csservermanager.properties.ApplicationProps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsservermanagerApplication {
+
+  public static void main(String[] args) {
+    String appDir = "D:/CS_DIR";
+    if (args.length > 0) {
+      appDir = args[0];
+    }
+    ApplicationProps.getProperties(appDir);
+    SpringApplication.run(CsservermanagerApplication.class, args);
+  }
+
+}
