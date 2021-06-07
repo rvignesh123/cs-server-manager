1d4418a0c0a9fab10d9aeae682ae7b5e9586f106
vignesh <vignesh.srgm@gmail.com>
2021-06-07 22:13:32 +0530

    Added XTerm and Maps

--- src/main/react-js-frontend/src/components/Manager/Console.js
+++ src/main/react-js-frontend/src/components/Manager/Console.js
@@ -1,18 +1,71 @@
 import React from "react";
 
-import { Jumbotron, Row, Col } from "react-bootstrap";
+import {
+  Jumbotron,
+  Row,
+  Col,
+  Card,
+  ButtonToolbar,
+  Button,
+  ButtonGroup,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
+import { XTerm } from "xterm-for-react";
+import { FitAddon } from "xterm-addon-fit";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDraftingCompass,
+  faSignInAlt,
+  faTerminal,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Console(props) {
+  const fitAddon = new FitAddon();
+  const xtermRef = React.useRef(null);
+
+  React.useEffect(() => {
+    // You can call any method in XTerm.js by using 'xterm xtermRef.current.terminal.[What you want to call]
+    xtermRef.current.terminal.writeln("Hello, World!");
+  }, []);
+
   return (
     <Row>
       <Col lg={12} className={"margin-top"}>
-        <Jumbotron className="bg-dark text-white">
-          <h1>Manager Console</h1>
-          <blockquote className="blockquote mb-0">
-            <p>Build in progress</p>
-            <footer className="blockquote-footer">Test footer</footer>
-          </blockquote>
-        </Jumbotron>
+        <Card>
+          <Card.Body>
+            <Row>
+              <Col>
+                <div>
+                  <h3>
+                    {" "}
+                    <FontAwesomeIcon icon={faTerminal} /> Server Console
+                  </h3>
+                </div>
+              </Col>
+              <Col xs lg="2">
+                <ButtonToolbar aria-label="Toolbar with button groups">
+                  <ButtonGroup className="me-2" aria-label="First group">
+                    <Button>Start</Button> <Button>Stop</Button>
+                  </ButtonGroup>
+                </ButtonToolbar>
+              </Col>
+            </Row>
+
+            <XTerm ref={xtermRef} addons={[fitAddon]} />
+            <InputGroup className="mb-3">
+              <FormControl
+                required
+                placeholder="Execute a command"
+                aria-label="Execute a command"
+                aria-describedby="basic-addon2"
+              />
+              <Button variant="outline-secondary" id="button-addon2">
+                Run
+              </Button>
+            </InputGroup>
+          </Card.Body>
+        </Card>
       </Col>
     </Row>
   );
--- src/main/react-js-frontend/src/components/Manager/ManagerSideBar.js
+++ src/main/react-js-frontend/src/components/Manager/ManagerSideBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Nav } from "react-bootstrap";
+import { Row, Col, Nav, ListGroup } from "react-bootstrap";
 import "../../styles/Manager.css";
 import {
   BrowserRouter as Router,
@@ -11,30 +11,32 @@ import {
 } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faUserPlus,
+  faDraftingCompass,
   faSignInAlt,
-  faSignOutAlt,
+  faTerminal,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function ManagerSideBar(props) {
   let { path, url } = useRouteMatch();
   return (
     <Nav
-      className="col-md-12 d-none d-md-block bg-light sidebar"
-      activeKey="/home"
+      className="col-md-12 d-none d-md-block sidebar"
+      activeKey="/manager"
       onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
     >
       <div className="sidebar-sticky"></div>
-      <Nav>
-        <Link to={`${url}/console`}>
-          <FontAwesomeIcon icon={faSignInAlt} /> Console
-        </Link>
-      </Nav>
-      <Nav>
-        <Link to={`${url}/maps`}>
-          <FontAwesomeIcon icon={faSignInAlt} /> Change Map
-        </Link>
-      </Nav>
+      <ListGroup>
+        <ListGroup.Item>
+          <Link to={`${url}/console`} className="nav-link">
+            <FontAwesomeIcon icon={faTerminal} /> Console
+          </Link>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <Link to={`${url}/maps`} className="nav-link">
+            <FontAwesomeIcon icon={faDraftingCompass} /> Maps
+          </Link>
+        </ListGroup.Item>
+      </ListGroup>
     </Nav>
   );
 }
